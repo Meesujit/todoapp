896161131007e0856f1ae351c2c7c3b4ca81fe39
sujit gouda <sujit.gda997@gmail.com>
2024-07-26 12:57:12 +0530

    First commit

---
+++ components/Todo.js
@@ -0,0 +1,107 @@
+import React, { useState, useRef } from 'react'
+import TodoForm from './TodoForm'
+
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { CiEdit } from "react-icons/ci";
+import { MdDownloading } from "react-icons/md";
+
+import { Reorder } from 'framer-motion'
+
+
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+
+
+
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  })
+  const [item, setItem] = useState({ todos })
+
+  const pdfRef = useRef(null);
+
+  const handleGeneratePdf = async () => {
+    const inputData = pdfRef.current;
+
+    try {
+      const canvas = await html2canvas(inputData);
+      const imgData = canvas.toDataURL('image/png');
+
+      const pdf = new jsPDF({
+        orientation: 'landscape',
+        unit: 'px',
+        format: 'a4'
+      })
+      
+      const width = pdf.internal.pageSize.getWidth();
+      const height = (canvas.height * width) / canvas.width;
+
+      pdf.addImage(imgData, "PNG", 0 , 0, width, height);
+      pdf.save('Document.pdf')
+    } catch (error) {
+      console.log('Error while creating the pdf: '+error)
+    }
+  }
+
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value)
+    setEdit({
+      id: null,
+      value: ''
+    })
+  }
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />
+  }
+
+
+
+
+  return (
+    <div ref={pdfRef}>
+      <Reorder.Group
+        values={item}
+        onReorder={setItem}
+        axis='y'
+      >
+        {todos.map((todo, index, item) => (
+          <Reorder.Item
+            item={item}
+            key={item}>
+
+              {/* CARD CONTENT  */}
+              <div
+                className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+                key={index}
+              >
+                <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                  <p className='font-mono text-lg' >
+                  {todo.text}
+                  </p>
+                </div>
+                <div className='icons'>
+                  <IoCloseCircleOutline
+                    onClick={() => removeTodo(todo.id)}
+                    className='delete-icon'
+                  />
+                  <CiEdit
+                    onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                    className='edit-icon'
+                  />
+                  <MdDownloading className='' onClick={handleGeneratePdf}/>
+                </div>
+              </div>
+          </Reorder.Item>
+        ))}
+      </Reorder.Group>
+    </div>
+
+  )
+}
+
+export default Todo
---
+++ components/TodoForm.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+  const inputRef = useRef(null);
+
+  try {
+    useEffect(() => {
+      inputRef.current.focus();
+    });
+    
+  } catch (error) {
+    console.log(error);
+  }
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+    setInput('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='todo-form flex justify-center items-center gap-2'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder='Update your item'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit rounded-[5px]'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Update
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder='Add a todo'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Submit
+          </button>
+        </>
+      )}
+    </form>
+  );
+}
+
+export default TodoForm;
